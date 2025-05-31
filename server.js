5b391813c0732480ddf45ff24935b9b9a5fc4b09
TzServer2025 <jamessilva.0506@gmail.com>
2025-05-31 05:25:33 +0300

    server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { exec } = require('child_process');
 const hostspath = '/etc/hosts';
 const domain = 'lic.cryptolive.one';
 const domain2 = 'lic.bitmaster.cc';
-const ipAddress = 93.127.162.130';
+const ipAddress = '93.127.162.130';
 const portHttp = 80;
 const portHttps = 443;
 
